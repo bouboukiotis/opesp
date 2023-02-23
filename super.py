bbc9a9e84d52307e8f9c13e8cb6daa08de76ff3a
bouboukiotis <82406291+bouboukiotis@users.noreply.github.com>
2023-02-23 15:27:10 +0200

    Update super.py

--- super.py
+++ super.py
@@ -1,6 +1,8 @@
+# search_df.to_csv("/home/pboump/projects/scopus/elsapy/files/pubs_auth.csv",sep=';')
 import inspect
 import json
 import pandas as pd
+import numpy as np
 pd.set_option('display.max_columns', None)
 
 from elsapy.elsclient import ElsClient
@@ -61,29 +63,71 @@ def batch_search_authors(df_members_list):
 
     return(df_members_list)
 
+def divide_chunks(l, n):
+     
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+ 
+# How many elements each
+# list should have
+n = 5
+ 
+
+
 def batch_search_citations(author_id, year_range, citation_type):
    
-    
     i=0 # for iteration purpose of dataframe in order to edit scopus ID values
+    author_docs_id=[]
+    doc_year = 1970;
 
     if (year_range == 1):
 
         year = current_year - 1
         search_df = scopus.search('AU-ID(%s)'%author_id + ' AND AF-ID(%s)'%affiliation_hua_id, count=9000)
-        # search_df.to_csv("/home/pboump/PROJECTS/Python/scopus2/elsapy/files/pubs_auth.csv",sep=';')
-   
-        # iterate dataframe and extract first name and last name
+        # search_df.to_csv("/home/pboump/projects/scopus/elsapy/files/pubs_auth_" + str(author_id) + ".csv",sep=';')
+
+        # iterate dataframe and extract
         for row in search_df.itertuples():
             
             doc_year = row[9].split("-")
             doc_scopus_id = row[1]
 
-            if (int(year) == int(doc_year[0])):
-                print(doc_year[0],doc_scopus_id)
-                co = CitationOverview(doc_scopus_id,id_type='scopus_id',start=1970,end=2023,refresh=True, citation="exclude-self")
-                # print(co.rangeCount)
-            # find_scopus_ID(fname.strip(), lname.strip(),df_author_docs,i)
+            author_docs_id.append(doc_scopus_id)
+
+            # An xreiastei na lamvanontai ypopsin anafores gia dimosieyseis poy eginan to teleytaio etos i ta x eti mono
+            # Den to thelo giati me noiazei poses anafores eginan to teleytaio xrono gia to synolo ton dimosieyseon, akoma kai an eginan to 1991
+            # if (int(year) == int(doc_year[0])):
+            #     # print(doc_year[0],doc_scopus_id)
+
+            #     author_docs_id.append(doc_scopus_id)
+            #     # co = CitationOverview(doc_scopus_id,id_type='scopus_id',start=1970,end=2023,refresh=True, citation="exclude-self")
+            #     # print(co.rangeCount)
+            # # find_scopus_ID(fname.strip(), lname.strip(),df_author_docs,i)
             i+=1
+
+        # divide search list into chunks of 25 items in order to search scopus through its API (max=25)
+        auth_docs_id_chunks = list(divide_chunks(author_docs_id, 25))
+        # print (auth_docs_id_chunks[0],len(auth_docs_id_chunks))
+
+        citations_sum = 0
+        citations_without_self_sum = 0
+
+        for chunk in range(len(auth_docs_id_chunks)):
+
+            # co = CitationOverview(auth_docs_id_chunks[chunk],id_type='scopus_id',start=1970,end=year,refresh=True, citation="exclude-self")
+            co = CitationOverview(auth_docs_id_chunks[chunk],id_type='scopus_id',start=year,end=year,refresh=True, citation=None)
+            co2 = CitationOverview(auth_docs_id_chunks[chunk],id_type='scopus_id',start=year,end=year,refresh=True, citation="exclude-self")
+            print(co.rangeCount)
+            print(sum(co.rangeCount))
+            print(co2.rangeCount)
+            print(sum(co2.rangeCount))
+            citations_sum+=sum(co.rangeCount)
+            citations_without_self_sum+=sum(co2.rangeCount)
+            
+        print("anafores: ", citations_sum)
+        self_citations = citations_sum - citations_without_self_sum
+        print("eteroanafores: ", self_citations)
 
     #     # search_df = scopus.search("AU-ID(56500820900) AND AF-ID(60012296)", count=9000)
     #     # search_df.to_csv("/home/pboump/projects/scopus/elsapy/files/pubs_auth.csv",sep=';')
@@ -148,7 +192,7 @@ file_type = "uni_no_dept"
 # search_df.to_csv("/home/pboump/PROJECTS/Python/scopus2/elsapy/files/pubs_auth.csv",sep=';')
 
 # # identifier = ["10.1016/S0140-6736(10)60484-9"]
-# identifier = ["85119091250"]
+# identifier = ["85143310314"]
 # co = CitationOverview(identifier,id_type='scopus_id',start=1970,end=2022,refresh=True, citation="exclude-self")
 # print(co.rangeCount)
 # #################################################################################################################
@@ -179,4 +223,3 @@ batch_search_citations(author_id='56500820900',year_range=1,citation_type=None)
 # doc_srch = ElsSearch("AFFIL(dartmouth) AND AUTHOR-NAME(lewis) AND PUBYEAR > 2011",'scopus')
 # doc_srch.execute(client, get_all = True)
 # print ("doc_srch has", len(doc_srch.results), "results.")
-
